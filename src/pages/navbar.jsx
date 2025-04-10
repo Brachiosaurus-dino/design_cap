9ebbc3b43d1ac04a99066b42f4bffd19ae16e90c
Brachiosaurus-dino <maniidon7890@gmail.com>
2025-04-10 17:56:57 +0530

    Update navbar.jsx

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
-            <div className=" text-center " >
+              <div className=" text-center " >
 
                 <img className="  rounded-md scale-100 transition-all duration-300 object-cover object-position-[50%_50%] " src="/caps_images/hop.jpg" />
                 <div className="absolute inset-0 flex items-center justify-center text-white text-6xl font-bold font-roboto">
@@ -28,17 +28,17 @@ const Navbar = () => {
                                 <a href="#" className="flex p-0 items-start justify-start space-x-3 rtl:space-x-reverse">
                                     <img src="/caps_images/9ed5e315-af6c-47d2-8f0f-ca7d3f12e044.png" className="h-20" alt="Flowbite Logo" />
                                    
-                                    <span className="self-center font-bold text-2xl drop-shadow-sm whitespace-nowrap font-roboto dark:text-white">Topperz</span>
+                                    <span className="self-center font-bold text-2xl drop-shadow-sm whitespace-nowrap font-roboto dark:text-black">Topperz</span>
                                     
                                 </a>
                                 <div className="flex md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
                                     <div className="justify-start hidden  md:flex md:w-auto " id="navbar-sticky">
                                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0    ">
                                             <li>
-                                                <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm font-roboto hover:bg-gray-100 md:hover:bg-transparent  md:p-0  dark:text-white  dark:hover:text-white md:dark:hover:bg-transparent   hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Login</a>
+                                                <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm font-roboto hover:bg-gray-100 md:hover:bg-transparent  md:p-0   dark:hover:text-white md:dark:hover:bg-transparent   hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Login</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="block py-2 px-3 font-roboto text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:p-0  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 relative  hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Registration</a>
+                                                <a href="#" className="block py-2 px-3 font-roboto text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:p-0   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 relative  hover:text-gray-400 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Registration</a>
                                             </li>
                                         </ul>
                                     </div>
